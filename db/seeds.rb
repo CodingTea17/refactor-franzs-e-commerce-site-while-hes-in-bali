c571b8fc36c78ff77bd8b0b9a480bb27355ee3bb
Dawson M <dawson.mortenson@gmail.com>
2017-12-08 13:18:03 -0800

    add an admin to the seeds and create a conditional link for admins to get to the new product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
-class Seed
+User.destroy_all
 
+class Seed
   def self.begin
     seed = Seed.new
     seed.generate_products
+    seed.conjure_admin
   end
 
   def generate_products
     20.times do |i|
       Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
     end
+  end
+
+  def conjure_admin
+    User.create!(:name => "AddyAdmin", :email => "admin@admin.com", :password => "password", :admin => true)
   end
 end
 
